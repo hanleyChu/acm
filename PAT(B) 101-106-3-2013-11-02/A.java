46d96c72773e0a1b8781cfc8768b3b99a68816f4
hyzhu <389059236@qq.com>
2013-12-26 01:22:38 +0800

    PAT basic 1016 AC

--- PAT(B) 101-106-3-2013-11-02/A.java
+++ PAT(B) 101-106-3-2013-11-02/A.java
@@ -1,15 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+
 public class A
 {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        Scanner in = new Scanner(new BufferedInputStream(A.class.getClassLoader().getResourceAsStream("test")));
 
-        String[]  parts = in.next().trim().split("\\s");
-
-        String a = parts[0];
-        char da = parts[1].charAt(0);
-        String b = parts[2];
-        char db = parts[3].charAt(1);
+        String a = in.next();
+        byte da = in.nextByte(); 
+        String b = in.next();
+        byte db = in.nextByte(); 
 
         int la = a.length();
         int lb = b.length();
@@ -17,21 +19,19 @@ public class A
         int ca = 0,cb = 0;
         for(int j =0;j<la;++j)
         {
-            if(a.charAt(j)==da)
+            if(a.charAt(j)==da+48)
                 ca = ca*10+1;
         }
 
         for(int j =0;j<lb;++j)
         {
-            if(b.charAt(j)==db)
+            if(b.charAt(j)==db+48)
                 cb = cb*10+1;
         }
 
-        ca = ca*((int)a-48);
-        cb = cb*((int)b-48);
+        ca = ca*((int)da);
+        cb = cb*((int)db);
 
         System.out.println(ca+cb);
-
-
     }
 }
